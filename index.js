40570ac97f3b7b0f5f330fb1feee6baa8beef0eb
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-28 19:06:19 +0000

    rewrite

--- index.js
+++ index.js
@@ -1,71 +1,46 @@
-var handshake = require('pull-handshake')
 var crypto = require('crypto')
-var through = require('pull-through')
 var pull = require('pull-stream')
 var deferredThrough = require('./resume')
 var header = require('./header')
 var peek = require('./peek')
 var cipher = require('./cipher')
 
-function isString(s) {
-  return 'string' === typeof s
-}
-
-var m = 0
 module.exports = function (dh, alg) {
 
   dh = dh || 'modp5'
   alg = alg || 'aes-256-cbc'
   return function (stream) {
-    var k = ++m
-    console.log('wrapStream', k)
-    var encrypt = crypto.getDiffieHellman(dh)
-    var decrypt = crypto.getDiffieHellman(dh)
+    var encryptDH = crypto.getDiffieHellman(dh)
+    var decryptDH = crypto.getDiffieHellman(dh)
 
-    encrypt.generateKeys()
-    decrypt.generateKeys()
+    encryptDH.generateKeys()
+    decryptDH.generateKeys()
 
-    var len = encrypt.getPublicKey().length
-    var deferred = pull.defer()
+    var length = encryptDH.getPublicKey().length
 
-    var decryptS, encryptS
-
-    var sEncrypt = cipher.encrypt(alg)
-    var sDecrypt = cipher.decrypt(alg)
+    var encrypt = cipher.encrypt(alg)
+    var decrypt = cipher.decrypt(alg)
 
     var keys = Buffer.concat([
-          encrypt.getPublicKey(),
-          decrypt.getPublicKey()
+          encryptDH.getPublicKey(),
+          decryptDH.getPublicKey()
         ])
-
-    var i = 0, j = 0
-
-    var seen = {}
 
     return {
       sink: pull(
-        header(len*2, function (head) {
-          sDecrypt.secret(decrypt.computeSecret(head.slice(0, len)))
-          sEncrypt.secret(encrypt.computeSecret(head.slice(len, len*2)))
+        header(length*2, function (head) {
+          decrypt.secret(decryptDH.computeSecret(head.slice(0, length)))
+          encrypt.secret(encryptDH.computeSecret(head.slice(length, length*2)))
         }),
         peek(),
-        pull.through(function (data) {
-          var d = data.toString('hex')
-          console.log('W', k, ++j, data.length, data.toString('hex'))
-          if(seen[d]) throw new Error('already seen data:' + d)
-          seen[d] = true
-        }),
-        sDecrypt,
+        decrypt,
         stream.sink
       ),
       source:
         pull(
           stream.source,
-          sEncrypt,
-          pull.prepend(keys),
-          pull.through(function (data) {
-            console.log('R', k, ++i, data.length, data.toString('hex'))
-          })
+          encrypt,
+          pull.prepend(keys)
         )
     }
   }
