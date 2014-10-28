dae6594fcd3f8219800d17592fa1419d176389ab
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-28 18:37:18 +0000

    add test coverage for using peek with deferred through - this was causing problems

--- test/peek.js
+++ test/peek.js
@@ -1,6 +1,7 @@
 var pull = require('pull-stream')
 var tape = require('tape')
 var peek = require('../peek')
+var deferred = require('../resume')
 
 tape('peek ahead', function (t) {
   pull(
@@ -31,4 +32,28 @@ tape('peek ahead and passthrough', function (t) {
   )
 })
 
+tape('peek with resume', function (t) {
 
+  var defer = deferred()
+
+  pull(
+    pull.values([1,2,3,4,5]),
+    defer,
+    peek(function (end, data) {
+      console.log('first', end, data)
+      t.equal(data, 2)
+      first = data
+    }),
+    pull.collect(function (err, ary) {
+      if(err) throw err
+      t.equal(first, 2)
+      t.deepEqual(ary, [2,4,6,8,10])
+      t.end()
+    })
+  )
+
+  defer.resolve(pull.map(function (e) {
+    return e*2
+  }))
+
+})
