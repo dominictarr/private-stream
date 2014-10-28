2845400973830baae572c9434998b5b0c46ed35f
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-28 18:20:11 +0000

    more tests

--- test/header.js
+++ test/header.js
@@ -3,6 +3,7 @@ var tape  = require('tape')
 var header = require('../header')
 var crypto = require('crypto')
 var split  = require('pull-randomly-split')
+var cat = require('pull-cat')
 
 tape('grab the first N items from a stream', function (t) {
   var head
@@ -37,7 +38,7 @@ tape('end outer stream when there is an incomplete header', function (t) {
 
 })
 
-for(var i = 1; i <= 20; i++)
+for(var i = 1; i <= 1; i++)
   tape('shoud work with randomly split data ' + i, function (t) {
     var head = crypto.pseudoRandomBytes(256)
     var body = crypto.pseudoRandomBytes(1024)
@@ -45,15 +46,42 @@ for(var i = 1; i <= 20; i++)
 
     pull(
       pull.values([head, body]),
-      split(32, 512),
+//      split(32, 512),
+      split(),
       header(256, function (a) {
+        t.equal(a.length, head.length)
         t.deepEqual(a, head)
         _a = a
       }),
       pull.collect(function (err, a) {
-        t.deepEqual(Buffer.concat(a), body)
+        t.deepEqual(Buffer.concat(a), Buffer.concat([body]))
         t.deepEqual(_a, head)
         t.end()
       })
     )
   })
+
+for(var i = 1; i <= 20; i++)
+  tape('shoud work with randomly split data ' + i, function (t) {
+    var head = crypto.pseudoRandomBytes(192)
+    var body = crypto.pseudoRandomBytes(0)
+    var _a
+
+    pull(
+      cat([
+        pull.values([head]),
+        function (abort, cb) {
+          t.deepEqual(_a, head)
+          t.end()
+          
+        }
+      ]),
+      split(),
+      header(192, function (a) {
+        t.deepEqual(a, head)
+        _a = a
+      }),
+      pull.drain()
+    )
+  })
+
