770f8c7359c0b80834cbedf263308d89030b1534
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-29 01:02:03 +0000

    run tests with different crypto settings

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
-var private = require('./')() //use defaults.
 var pull = require('pull-stream')
+var random = require('pull-randomly-split')
+var tape = require('tape')
 
 function stretch(msg) {
-  var l = 1, a = []
+  var l = 10, a = []
   while(l--) a.push(msg)
   return a
 }
@@ -13,22 +14,79 @@ function endpoint (messages, cb) {
     sink: pull(
       pull.map(String),
       pull.collect(function (err, ary) {
+        console.log(ary)
         cb(err, ary && ary.join(''))
       })
     )
   }
 }
 
-var alice = endpoint(stretch('hello bob, from alice...'), function (err, ary) {
-  console.log('alice recieved:', ary)
-})
 
-var bob = endpoint(stretch('hello alice, from bob!'), function (err, ary) {
-  console.log('bob recieved:', ary)
-})
+function tests(mod, aes) {
 
-var aliceCipher = private(alice)
-var bobCipher = private(bob)
+  var private = require('../')(mod, aes) //use defaults.
 
-pull(aliceCipher, bobCipher, aliceCipher)
+
+  tape('encrypted duplex stream', function (t) {
+
+    t.plan(2)
+
+    var a = 'hello bob from alice'
+    var b = 'hello alice from bob'
+    var alice = endpoint([a], function (err, str) {
+      console.log('alice recieved:', str)
+      t.equal(str, b)
+    })
+
+    var bob = endpoint([b], function (err, str) {
+      console.log('bob recieved:', str)
+      t.equal(str, a)
+    })
+
+    var aliceCipher = private(alice)
+    var bobCipher = private(bob)
+
+    pull(
+      bobCipher,
+      aliceCipher,
+      bobCipher
+    )
+
+  })
+
+  for(var i = 0; i < 100; i++)
+  tape('encrypted duplex stream - random buffers - ' + i, function (t) {
+
+    t.plan(2)
+
+    var a = 'hello bob from alice'
+    var b = 'hello alice from bob'
+    var alice = endpoint([a], function (err, str) {
+      console.log('alice recieved:', str)
+      t.equal(str, b)
+    })
+
+    var bob = endpoint([b], function (err, str) {
+      console.log('bob recieved:', str)
+      t.equal(str, a)
+    })
+
+    var aliceCipher = private(alice)
+    var bobCipher = private(bob)
+
+    pull(
+      bobCipher,
+    //  random(4, 32),
+      aliceCipher,
+  //    random(4, 32),
+      bobCipher
+    )
+
+  })
+
+}
+
+tests('modp1', 'aes-128-cbc')
+tests('modp2', 'aes-192-cbc')
+tests('modp5', 'aes-256-cbc')
 
