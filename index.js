d6c6b939d0c2d6fd548ed224899472209908364b
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-29 14:13:46 +0100

    comment on thing I am not sure about

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ var peek = require('pull-peek')
 var cipher = require('./cipher')
 
 function genDH (mod, len) {
+  /*
+    generate a DH key that is not too short.
+    I'm not really sure if this is the right way to do this,
+    or why DH is sometimes not even. Without this,
+    sometimes tests fail, and I don't want to use length delimiting
+    so that all the bytes are random and it does not give away
+    what cipher is being used.
+  */
   var dh
   do {
     dh = crypto.getDiffieHellman(mod)
