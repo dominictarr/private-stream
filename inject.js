6055020f3a9d64ba2ff6b052a6bf04cd284ef483
Dominic Tarr <dominic.tarr@gmail.com>
2015-03-07 22:58:52 +1300

    tidy

--- inject.js
+++ inject.js
@@ -1,38 +1,36 @@
-var pull               = require('pull-stream')
-var header             = require('pull-header')
-var peek               = require('pull-peek')
+var pull   = require('pull-stream')
+var header = require('pull-header')
+var peek   = require('pull-peek')
 
-module.exports =
-  function (createKeyExchange, createStreamCipher, createHash) {
+module.exports = function (KeyExchange, StreamCipher, Hash) {
 
-    return function (stream) {
-      var exchange = createKeyExchange()
-      var local    = exchange.getPublicKey()
+  return function (stream) {
+    var exchange = KeyExchange()
+    var local    = exchange.getPublicKey()
 
-      var encrypt  = createStreamCipher()
-      var decrypt  = createStreamCipher()
+    var encrypt  = StreamCipher()
+    var decrypt  = StreamCipher()
 
-      function hash (a, b) {
-        return createHash().update(a).update(b).digest()
-      }
+    function hash (a, b) {
+      return Hash().update(a).update(b).digest()
+    }
 
-      return {
-        sink: pull(
-          header(local.length, function (remote) {
-            var secret = exchange.computeSecret(remote)
-            decrypt.secret(hash(remote, secret))
-            encrypt.secret(hash(local, secret))
-          }),
-          peek(),
-          decrypt,
-          stream.sink
-        ),
-        source: pull(
-          stream.source,
-          encrypt,
-          pull.prepend(local)
-        )
-      }
+    return {
+      sink: pull(
+        header(local.length, function (remote) {
+          var secret = exchange.computeSecret(remote)
+          decrypt.secret(hash(remote, secret))
+          encrypt.secret(hash(local, secret))
+        }),
+        peek(),
+        decrypt,
+        stream.sink
+      ),
+      source: pull(
+        stream.source,
+        encrypt,
+        pull.prepend(local)
+      )
     }
   }
-
+}
